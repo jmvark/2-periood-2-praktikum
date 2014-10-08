c4ea8b8bd9825ea2125374fc7d5a9db77bad59c8
JasonMax_2 <jason-max.vark@khk.ee>
2014-10-08 10:42:59 +0300

    initial commit

---
+++ components/html-head.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;
+charset=utf-8">
+<title>2periood-2praktikum</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<head>
+<?php include(components/html-head.php); ?>
+</head>
+
+<body>
+
+</body>
+</html>
